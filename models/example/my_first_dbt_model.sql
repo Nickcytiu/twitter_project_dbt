a85278a83b9b49f4c6a9b140849189ed9359596d
nickcytiu <nickcytiu@gmail.com>
2023-02-21 15:41:45 -0500

    testing

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,13 +1,11 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='table',
+        dataset = "twitter_dbt",
+        enabled = true,
+        target = "prod"
+    ) 
+}}
 
 with source_data as (
 
@@ -24,4 +22,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+where id is not null
